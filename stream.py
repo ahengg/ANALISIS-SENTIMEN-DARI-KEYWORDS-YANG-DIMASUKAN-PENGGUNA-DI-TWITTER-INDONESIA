951ebc2b171cc4782a5670297c57dfff9c0a3f4f
Andrianto <57657621+ahengg@users.noreply.github.com>
2022-01-25 17:14:10 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -25,11 +25,7 @@ from datetime import date, timedelta
 import requests
 import pandas as pd  
 import urllib.request  
-
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
+ 
 ##
 
 import snscrape.modules.twitter as sntwitter 
